389e90b8f6645478903ba50397a6ca153cfa3136
Admond Tamang <admondtamang@gmail.com>
2021-06-16 16:46:16 +0545

    [add] boiler palte

---
+++ android/app/src/main/kotlin/com/example/nepe_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nepe_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/routes.dart
@@ -0,0 +1,7 @@
+class MyRoutes {
+  static String homeRoute = "/";
+  static String loginRoute = "/login";
+  static String homeDetailsRoute = "/detail";
+  static String cartRoute = "/cart";
+  static String rootRoute = "/root";
+}
