3742187fffc9df6dd69400af12f3ce0499975059
DhakalRupesh <a73dhakal@outlook.com>
2021-06-24 10:58:10 +0545

    mipmap image added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nepe_app/provider/google_sign_in.dart';
-import 'package:nepe_app/screens/Login/LoginPage.dart';
 import 'package:nepe_app/screens/Login/login_home.dart';
 import 'package:nepe_app/utils/constants.dart';
-import 'package:nepe_app/screens/Home/HomeScreen.dart';
-import 'package:nepe_app/screens/Navigation/BottomNav.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
